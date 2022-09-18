e07616e357db2dbf062b1d6938cd1debfcf237f0
navakodesh <112860194+navakodesh@users.noreply.github.com>
2022-09-18 10:43:53 +0300

    Add files via upload

--- app_contery.js
+++ app_contery.js
@@ -2,51 +2,55 @@ import { Contery } from "./class_contery.js";
 import { declareEvents } from "./viewEvent.js";
 
 
-const init=()=>{
+const init = () => {
     doApi()
     declareEvents(doApi)
 }
 
 
-const doApi=async(_searchQuery)=>{
-    try{
+const doApi = async (_searchQuery) => {
+    try {
 
-        if(_searchQuery){
-      let url=  `https://restcountries.com/v3.1/name/${_searchQuery}`;
-        let resp=await fetch(url)
-        let data=await resp.json()
-        createContery(data)
+        if (_searchQuery) {
+            let url = `https://restcountries.com/v3.1/name/${_searchQuery}`;
+            let resp = await fetch(url)
+            let data = await resp.json()
+            createContery(data)
         }
 
-        else if(!_searchQuery){
-            const urlParams=new URLSearchParams(window.location.search);
-            let name=urlParams.get("name");
-            if(name){
-            let url=`https://restcountries.com/v3.1/name/${name}`;
-            let resp=await fetch(url)
-            let data=await resp.json()
-            createContery(data)
-            }
-        
-            else{
-               let url=`https://restcountries.com/v3.1/name/israel`
-                let resp=await fetch(url)
-                let data=await resp.json()
+        else if (!_searchQuery) {
+            const urlParams = new URLSearchParams(window.location.search);
+            let name = urlParams.get("name");
+            if (name) {
+                let url = `https://restcountries.com/v3.1/name/${name}`;
+                let resp = await fetch(url)
+                let data = await resp.json()
                 createContery(data)
             }
-        
-            }}
-    catch(err){
+
+            else {
+                let url = `https://restcountries.com/v3.1/name/israel`
+                let resp = await fetch(url)
+                let data = await resp.json()
+                createContery(data)
+            }
+
+        }
+    }
+    catch (err) {
         console.log(err)
-        alert("Country name unknown")
+        document.querySelector("#id_row").innerHTML = 
+        `<h2 class="display-2">Country name unknown</h2>`;
+
+       
 
     }
 }
 
 export const createContery = (_ar) => {
     document.querySelector("#id_row").innerHTML = "";
-      let contery = new Contery("#id_row",_ar[0],_ar);
-      contery.render();
+    let contery = new Contery("#id_row", _ar[0], _ar);
+    contery.render();
 }
 
 
--- class_contery.js
+++ class_contery.js
@@ -1,27 +1,27 @@
-export class Contery{
-    constructor(_parent,_item){
-        this.parent=_parent;
-        this.common=_item.name.common;
-        this.population=_item.population;
-        this.region=_item.region;
-        this.languages=Object.keys(_item.languages);
-        this.coin=Object.keys(_item.currencies);
-        this.coinD=Object.values(_item.currencies)[0].name;
-        this.capital=_item.capital;
-        this.svg=_item.flags.svg;
-        this.googleMaps=_item.maps.googleMaps;
-        this.borders=_item.borders;
-        this.latlng=_item.capitalInfo.latlng;
+export class Contery {
+    constructor(_parent, _item) {
+        this.parent = _parent;
+        this.common = _item.name.common;
+        this.population = _item.population;
+        this.region = _item.region;
+        this.languages = Object.keys(_item.languages);
+        this.coin = Object.keys(_item.currencies);
+        this.coinD = Object.values(_item.currencies)[0].name;
+        this.capital = _item.capital;
+        this.svg = _item.flags.svg;
+        this.googleMaps = _item.maps.googleMaps;
+        this.borders = _item.borders;
+        this.latlng = _item.capitalInfo.latlng;
     }
 
 
-    render(){
-        let div=document.createElement("div");
+    render() {
+        let div = document.createElement("div");
         document.querySelector(this.parent).append(div)
-        div.innerHTML= `
+        div.innerHTML = `
         <div class="col-12">
-        <img class="col-6 m-5 float-start" src="${this.svg}" alt="flag">
-        <div class="col-6 mt-5 float-end">
+        <img class="col-md-6 m-5 float-start" src="${this.svg}" alt="flag">
+        <div class="p col-12 col-md-6 mt-5 float-end">
         <h2>${this.common}</h2>
         <p>pop: ${this.population}<br>
         region: ${this.region} <br>
@@ -33,24 +33,22 @@ export class Contery{
 
         <div class="myD col-12">
 
-        <iframe class="col-6 m-5" src="https://maps.google.com/maps?q=${this.latlng}&z=7&output=embed"width="300" height="350" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade" floar-start m-2></iframe>
-        <h5 class="col-4 mt-5 float-end">States with borders:<span>
-        <div class="b_d"></div>
-        </span></h5>
+        <iframe class="col-md-6 m-5" src="https://maps.google.com/maps?q=${this.latlng}&z=7&output=embed"width="300" height="350" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade" floar-start m-2></iframe>
+        <h5 class="col-12 col-md-4 mt-5 float-end">States with borders:<div class="b_d"></div></h5>
 
         </div>
 
         `
-        let b_d=div.querySelector(".b_d")
+        let b_d = div.querySelector(".b_d")
         this.borders.forEach(item => {
-            let span=document.createElement("span")
-            span.innerHTML+= `<a class="float-start p-1 list-group-item" href="index.html?name=${item}">${item}. </a>`
-       b_d.append(span)
+            let span = document.createElement("span")
+            span.innerHTML += `<a class="float-start p-1 list-group-item" href="index.html?name=${item}">${item}. </a>`
+            b_d.append(span)
         });
     }
 
 
-   
+
 }
 
 
